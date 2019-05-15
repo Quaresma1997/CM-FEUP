39c9e6bc50e1e9a1caa24206711e80e4045cf54e
Luís Correia <up2015033@fe.up.pt>
2019-05-15 18:55:53 +0100

    Weather app

---
+++ WeatherApp/WeatherApp/WeatherApp.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "f1f5d2fb-f562-479d-a0c3-7216eedbd7ec",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
