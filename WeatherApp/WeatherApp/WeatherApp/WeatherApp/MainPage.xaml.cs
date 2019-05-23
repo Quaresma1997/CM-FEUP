64feeab0b913f1a03610a6c57311c568d6b3f031
Luís Correia <up2015033@fe.up.pt>
2019-05-23 22:56:38 +0100

    gets different cities weather info

---
+++ WeatherApp/WeatherApp/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        
+        public MainPage()
+        {
+ 
+            InitializeComponent();
+            CitiesPage.ListView.ItemSelected += ListView_ItemSelected;
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as CityItem;
+
+            if(item != null)
+            {
+
+              /*  var page = (WeatherPage)Activator.CreateInstance(typeof(WeatherPage));
+                page.Title = item.Title;
+                page.setCity(item.Title);*/
+
+                var page = new WeatherPage(item.Title);
+                page.Title = item.Title;
+
+                Detail = new NavigationPage(page);
+                IsPresented = false;
+
+                CitiesPage.ListView.SelectedItem = null;
+            }
+               
+          
+        }
+    }
+}
